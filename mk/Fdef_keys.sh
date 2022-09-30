33a84e1022eb44dd27efdec516ed90dece9bbae1
Sebastien Masson <sebastien.masson@locean.ipsl.fr>
2022-09-30 14:14:06 +0000

    make clean working again in makenemo, #68

---
+++ mk/Fdef_keys.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+######################################################
+# Author : 
+# Contact : 
+#
+# Some functions called from makenemo
+# Fdef_keys   : define keys in cpp.fcm file  
+######################################################
+#set -x
+set -o posix
+#set -u
+#set -e
+#+
+#
+# ================
+# Fdef_keys.sh
+# ================
+#
+# --------------------------
+# Add compilation keys
+# --------------------------
+#
+# SYNOPSIS
+# ========
+#
+# ::
+#
+#  $ Fdef_keys.sh CONFIG_NAME def_key "LIST_KEYS"
+#
+#
+# DESCRIPTION
+# ===========
+#
+#
+# define cpp keys when compiling a configuration, key list has to be enclosed with " ".
+#
+# EXAMPLES
+# ========
+#
+# ::
+#
+#  $ ./Fdef_keys.sh CONFIG_NAME def_key "key_agrif"
+#
+#
+# TODO
+# ====
+#
+# option debug
+#
+#
+# EVOLUTIONS
+# ==========
+#
+#
+#   * creation
+#
+#-
+
+echo "Defining keys in : ${NEW_CONF}"
+
+echo " bld::tool::fppkeys ${list_def_key}" > ${CONFIG_DIR}/${NEW_CONF}/cpp_${NEW_CONF}.fcm
+
+unset -v list_def_key
